650c12680e4d05c8afc4c2224087f3856a1f5dd1
nourouhichi <1417@holbertonschool.com>
2021-08-19 00:04:02 +0100

    removing a prototype

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,9 +1,8 @@
-#ifndef _SANDPILES_H
-#define _SANDPILES_H
+#ifndef _SANDPILE_H
+#define _SANDPILE_H
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(void);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 int check(int grid[3][3]);
 void print(int grid[3][3]);
