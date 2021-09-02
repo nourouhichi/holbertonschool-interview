17586e5b03cdf70cf3b9b485451849cd8069abfd
nourouhichi <1417@holbertonschool.com>
2021-09-02 22:56:49 +0100

    new add

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -14,22 +14,22 @@ try:
     for line in sys.stdin:
         if p == 10:
             print("File size: {}".format(size))
-            for key, value in sorted(s.items()):
-                if value != 0:
-                    print("{}: {}".format(key, value))
+            for k, v in sorted(s.items()):
+                if v != 0:
+                    print("{}: {}".format(k, v))
             p = 1
         else:
             p += 1
         split = line.split()
         if len(split) > 2:
             size += int(split[-1])
-            for key, value in s.items():
-                if split[-2] == key:
-                    s[key] = s[key] + 1
+            for k, v in s.items():
+                if split[-2] == k:
+                    s[k] = s[k] + 1
 except KeyboardInterrupt:
     pass
 finally:
     print("File size: {}".format(size))
-    for key, value in sorted(s.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
+    for k, v in sorted(s.items()):
+        if v != 0:
+            print("{}: {}".format(k, v))
