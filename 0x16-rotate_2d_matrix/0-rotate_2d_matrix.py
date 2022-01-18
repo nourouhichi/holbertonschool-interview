afc93cbc88ccc60c4ae7cff62bedc202cad7ed95
nourouhichi <1417@holbertonschool.com>
2022-01-18 15:14:11 +0100

    add solution for mutability

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,13 +2,14 @@
 """
 Rotate 2D Matrix
 """
-import copy
 
 
 def rotate_2d_matrix(matrix):
     """2d matrix rotation"""
     k = len(matrix) - 1
-    tmp_mat = copy.deepcopy(matrix)
+    tmp_mat = []
+    for i in matrix:
+        tmp_mat.append(list(tuple(i)))
     for i in range(len(matrix)):
         for j in range(len(matrix)):
             tmp_mat[j][k] = matrix[i][j]
