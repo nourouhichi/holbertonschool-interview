897c49790c7d83f86437f5d5f8abba399d6c8f49
nourouhichi <1417@holbertonschool.com>
2022-05-25 12:43:02 +0100

    fixing

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -5,14 +5,14 @@
 def island_perimeter(grid):
     """Calculate perimeter of the island (grid)
     """
-    sum, limit = (0, 0)
+    sm, limit = (0, 0)
     grid_len, col = (len(grid), len(grid[0]))
     for i in range(grid_len):
-        sum += sum(grid[i])
+        sm += sum(grid[i])
         for j in range(col):
             if grid[i][j] :
                 if j > 0 and grid[i][j - 1] == 1:
                     limit += 1
                 if i > 0 and grid[i - 1][j] == 1:
                     limit += 1
-    return sum * 4 - limit * 2
+    return sm * 4 - limit * 2
